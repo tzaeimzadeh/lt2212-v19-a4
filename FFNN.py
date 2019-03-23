54d93b22e218dda329c828f0d450530ad371fc32
msdubrovski <elena.rraposo@gmail.com>
2019-03-23 19:00:54 +0100

    some changes in FFNN

--- FFNN.py
+++ FFNN.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import pandas
+import torch
 
 #https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
 class neuralNetwork:
@@ -14,29 +16,29 @@ class neuralNetwork:
         self.y = y
         self.output = np.zeros(y.shape)
 
-    def sigmoid(self, x):
-        self.x = x
-        return 1 / (1 + math.e**(-x))
-
-    def softmax(self, x):
-        self.x = x
-        return np.exp(x) / np.sum(np.exp(x), axis=0)
-
     def crossEntropy(self, yHat):
-        self.yHat = yHat
-        if self.y == 1:
+        self.yHat = yHat # ?
+        if y == 1: # self.y
             return -math.log(yHat)
         else:
             return -math.log(1-yHat)
 
     def feedForward(self):
-        self.layer1 = self.sigmoid(np.dot(self.weight1, self.input) + self.b)
-        self.layer2 = self.softmax(np.dot(self.weight2, self.layer1) + self.c)
+        self.layer1 = torch.sigmoid(torch.mul(self.weight1, self.X) + self.b)
+        self.layer2 = torch.softmax(torch.mul(self.weight2, self.layer1) + self.c)
 
-    def backpropagation(self):
-        #using Tensor / PyTorch (autograd) to do the differntiation
-        weight2_d = weight2.grad()
-        weight1_d = weight1.grad()
+    def train(self, X, y, epochs = 1): # L, M, N
+        self.weight1 = torch.randn(L, M, requires_grad=True)
+        self.b = torch.randn(M, requires_grad=True)
+        self.weight2 = torch.randn(M, N, requires_grad=True)
+        self.c = torch.randn(N, requires_grad=True)
+        # adam optimizer
+        # do i need to go element by element ?
+        self.feedForward()
+        # where do i get the y from? outside? 
+        loss = crossEntropy(y, yhat) # this should be a torch object?
+        loss.backward()
 
-        self.weight1 += weight1_d
-        self.weight2 += weight2_d
+
+
+# net = neuralNetwork()
