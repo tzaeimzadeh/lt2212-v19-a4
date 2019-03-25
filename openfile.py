51b6e68bf83fb2ce0d36b23eae509c7b658d27ef
Tala Zaeimzadeh <guszaeita@GU.GU.SE@mltgpu.flov.gu.se>
2019-03-25 11:56:55 +0100

    updates to openfile and preprocessdata

--- openfile.py
+++ openfile.py
@@ -1,15 +1,13 @@
 import os, argparse, gzip
 
 parser = argparse.ArgumentParser(description="Convert text to features")
-
 parser.add_argument("inputfile", type=str, help="The file name containing the text data.")
-
 args = parser.parse_args()
 
-<<<<<<< HEAD
+
 folder_path = os.listdir(args.inputfile)    #get file names from the source folder
-with gzip.open(os.path.join(args.inputfile, folder_path[0])) as source_file,
-gzip.open(os.path.join(args.inputfile, folder_path[1])) as target_file,
+with gzip.open(os.path.join(args.inputfile, folder_path[0])) as target_file,
+gzip.open(os.path.join(args.inputfile, folder_path[1])) as source_file,
 open("UN-french.txt", "w") as f1, open("UN-english.txt", "w") as f2:
     f1.write(source_file.read().decode('utf-8'))
     f2.write(target_file.read().decode('utf-8'))
--- processdata.py
+++ processdata.py
@@ -10,8 +10,8 @@ parser.add_argument("-E", "--end", metavar="E", dest="endline", type=int, defaul
                     help="What line of the input data file to end on. Default is None, whatever the last line is.")
 parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3,
                     help="The length of ngram to be considered (default 3).")
-parser.add_argument("inputfile", type=str, help="The file name containing the text data.")
-#parser.add_argument("outputfile", type=str, help="The name of the output file for the feature table.")
+parser.add_argument("inputfile", type=str, help="The folder name containing the text data.")
+parser.add_argument("outputfile", type=str, help="The name of the output file for the data (test or train)")
 
 args = parser.parse_args()
 
@@ -23,24 +23,30 @@ def preprocess_file():
         source_lang = list of lists, with each sublist representing a line in the source language file (French)
         target_lang = list of lists, with each sublist representing a line in the target language file (English)
     """
-    
+
     source_lang = []
     target_lang = []
 
     folder_path = os.listdir(args.inputfile)    #get file names from the source folder
-    with open(os.path.join(args.inputfile, folder_path[0]), encoding='utf-8') as source_file:
-        with open(os.path.join(args.inputfile, folder_path[1]), encoding='utf-8') as target_file:
-        #gzip.open()
-            for s_line, t_line in zip(source_file, target_file):                    #https://stackoverflow.com/questions/19007383/compare-two-different-files-line-by-line-in-python
-                preprocess_source = re.sub(r"[^\s\w]", " ", s_line.lower()).split(" ")
-                preprocess_target = re.sub(r"[^\s\w]", " ", t_line.lower()).split(" ")
-                preprocess_source = [x for x in preprocess_source if x]             #remove empty strings: https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
-                preprocess_target = [x for x in preprocess_target if x]
-                shortest = min(len(preprocess_source), len(preprocess_target))
-                truncated_source = preprocess_source[:shortest]
-                truncated_target = preprocess_target[:shortest]
-                source_lang.append(truncated_source)
-                target_lang.append(truncated_target)
+    with open(os.path.join(args.inputfile, folder_path[0]), 'r') as target_file, \
+            open(os.path.join(args.inputfile, folder_path[1]), 'r') as source_file:
+        for s_line, t_line in zip(source_file, target_file):                    #https://stackoverflow.com/questions/19007383/compare-two-different-files-line-by-line-in-python
+            preprocess_source = re.sub(r"[^\s\w]", " ", s_line.lower()).split(" ")
+            preprocess_target = re.sub(r"[^\s\w]", " ", t_line.lower()).split(" ")
+            preprocess_source = [x for x in preprocess_source if x]             #remove empty strings: https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
+            preprocess_target = [x for x in preprocess_target if x]
+            shortest = min(len(preprocess_source), len(preprocess_target))
+            truncated_source = preprocess_source[:shortest]
+            truncated_target = preprocess_target[:shortest]
+            source_lang.append(truncated_source)
+            target_lang.append(truncated_target)
+
+    smaller_source_lang = source_lang[args.startline:args.endline]
+    smaller_target_lang = target_lang[args.startline:args.endline]
+
+    with open("source-language-{}.txt".format(args.outputfile), "w") as source, open("target-language-{}.txt".format(args.outputfile), "w") as target:
+        source.write(" ".join(str(sent) for list in smaller_source_lang for sent in list))
+        target.write(" ".join(str(sent) for list in smaller_target_lang for sent in list))
 
     return source_lang, target_lang
 
